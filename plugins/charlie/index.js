b4804a20a3488c27004c58a7e24ae2ddc22ff646
daveandersononline <dave.anderson@dynatrace.com>
2017-07-03 17:50:08 +0100

    Create index.js

---
+++ plugins/charlie/index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ * The DavisWeather class is the core of the plugin and an
+ * instance of DavisWeather is what will be loaded into Davis
+ */
+class Hicharlie {
+
+  /**
+   * The main body of work is done in the constructor.
+   */
+  constructor(davis, options) {
+    this.davis = davis;
+    this.options = options;
+
+    // This is where we declare our intents.
+    this.intents = {
+      // Our intent name
+      hicharlie: {
+        // A basic description of the intent
+        usage: 'End the show',
+
+        // Phrases that will trigger our intent. Note that they will not
+        // need to be matched exactly in order for the intent to run.
+        phrases: [
+          'Can I have a star?',
+        ],
+
+        // Lifecycle Events are friendly names for the steps that an intent
+        // needs to take in order to run successfully. For instance, our intent
+        // will need to gather data from the weather underground API, then will
+        // need to respond to the user, so I have broken it up into those events.
+        lifecycleEvents: [
+          'gatherData',
+          'respond',
+        ],
+      },
+    };
+
+    // Hooks give intents functionality.
+    // They are called serially when an intent is run.
+    // They are named using the 'intentName:lifecycleEvent'
+    // Each hook is called with 2 arguments: the exchange object,
+    // and a context object. The exchange object is the primary
+    // interface between Davis, a user, and a plugin. The context
+    // object holds any state carried over from previous exchanges.
+    this.hooks = {
+      'hicharlie:gatherData': (exchange, context) => null,
+      'hicharlie:respond': (exchange, context) => {
+        const resp = 'Wow, you have been really good this week. Even better today so I think you can have a star. Please ask your mum or dad to give it to you as I have no hands.';
+
+        //var linkUrl = "https://coffeeconnectionhilliard.files.wordpress.com/2016/05/thank-you-1400x800-c-default.gif";
+	//      this.davis.server.pushLinkToUser(exchange.user, linkUrl, true);
+        
+        exchange
+          .response(resp) // respond to the user  
+          .smartEnd() // end the conversation if appropriate
+          .skipFollowUp();
+      },
+    };
+  }
+}
+
+// export the plugin so it can be used
+module.exports = Hicharlie;
