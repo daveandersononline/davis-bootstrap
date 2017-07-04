183d65a252db4f823ec25a8f0d1bb981ed195de2
daveandersononline <dave.anderson@dynatrace.com>
2017-07-04 09:26:14 +0100

    Update index.js

--- plugins/deploydynatrace/index.js
+++ plugins/deploydynatrace/index.js
@@ -23,7 +23,7 @@ class Deploydynatrace {
         // Phrases that will trigger our intent. Note that they will not
         // need to be matched exactly in order for the intent to run.
         phrases: [
-          'show me how to get started',
+          'how to deploy dynatrace',
         ],
 
         // Lifecycle Events are friendly names for the steps that an intent
