abea32d2025b32640aeae6edd0200355c035ab26
daveandersononline <dave.anderson@dynatrace.com>
2017-07-04 09:31:10 +0100

    Create index.js

---
+++ plugins/smartscapeservices/index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ * The DavisWeather class is the core of the plugin and an
+ * instance of DavisWeather is what will be loaded into Davis
+ */
+class Smartscapeservices {
+
+  /**
+   * The main body of work is done in the constructor.
+   */
+  constructor(davis, options) {
+    this.davis = davis;
+    this.options = options;
+
+    // This is where we declare our intents.
+    this.intents = {
+      // Our intent name
+      smartscapeservices: {
+        // A basic description of the intent
+        usage: 'End the show',
+
+        // Phrases that will trigger our intent. Note that they will not
+        // need to be matched exactly in order for the intent to run.
+        phrases: [
+          'Can I please see the smartscape view?',
+        ],
+
+        // Lifecycle Events are friendly names for the steps that an intent
+        // needs to take in order to run successfully. For instance, our intent
+        // will need to gather data from the weather underground API, then will
+        // need to respond to the user, so I have broken it up into those events.
+        lifecycleEvents: [
+          'gatherData',
+          'respond',
+        ],
+      },
+    };
+
+    // Hooks give intents functionality.
+    // They are called serially when an intent is run.
+    // They are named using the 'intentName:lifecycleEvent'
+    // Each hook is called with 2 arguments: the exchange object,
+    // and a context object. The exchange object is the primary
+    // interface between Davis, a user, and a plugin. The context
+    // object holds any state carried over from previous exchanges.
+    this.hooks = {
+      'smartscapeservices:gatherData': (exchange, context) => null,
+      'smartscapeservices:respond': (exchange, context) => {
+        const resp = 'hi dave, I am opening this up for you now. Getting started is really simple. Even someone with your human intelligence could probably explain it i am sure.';
+
+        var linkUrl = "https://jnc47888.live.dynatrace.com/#smartscape";
+	     this.davis.server.pushLinkToUser(exchange.user, linkUrl, true);
+        
+        exchange
+          .response(resp) // respond to the user  
+          .smartEnd() // end the conversation if appropriate
+          .skipFollowUp();
+      },
+    };
+  }
+}
+
+// export the plugin so it can be used
+module.exports = Smartscapeservices;
