e126f1b016cac6e0b1c73f438c83aec01983530b
daveandersononline <dave.anderson@dynatrace.com>
2017-07-17 12:55:16 +1000

    Create index.js

---
+++ plugins/performopen2/index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+/**
+ * The DavisWeather class is the core of the plugin and an
+ * instance of DavisWeather is what will be loaded into Davis
+ */
+class Performopen2 {
+
+  /**
+   * The main body of work is done in the constructor.
+   */
+  constructor(davis, options) {
+    this.davis = davis;
+    this.options = options;
+
+    // This is where we declare our intents.
+    this.intents = {
+      // Our intent name
+      performopen2: {
+        // A basic description of the intent
+        usage: 'End the show',
+
+        // Phrases that will trigger our intent. Note that they will not
+        // need to be matched exactly in order for the intent to run.
+        phrases: [
+          'thanks and so what are you going to contribute then?',
+        ],
+
+        // Lifecycle Events are friendly names for the steps that an intent
+        // needs to take in order to run successfully. For instance, our intent
+        // will need to gather data from the weather underground API, then will
+        // need to respond to the user, so I have broken it up into those events.
+        lifecycleEvents: [
+          'gatherData',
+          'respond',
+        ],
+      },
+    };
+
+    // Hooks give intents functionality.
+    // They are called serially when an intent is run.
+    // They are named using the 'intentName:lifecycleEvent'
+    // Each hook is called with 2 arguments: the exchange object,
+    // and a context object. The exchange object is the primary
+    // interface between Davis, a user, and a plugin. The context
+    // object holds any state carried over from previous exchanges.
+    this.hooks = {
+      'performopen2:gatherData': (exchange, context) => null,
+      'performopen2:respond': (exchange, context) => {
+        const resp = 'actually I am going to doing a demonstration with Alois showcasing my ability to identify the root cause of performance issues with my AI interlect. I can process billions of transactions in milliseconds and you are still shuffling your slides around at 7am this morning. Good luck.';
+
+      //  var linkUrl = "https://jnc47888.live.dynatrace.com/#install/agentlinux";
+	    // this.davis.server.pushLinkToUser(exchange.user, linkUrl, true);
+        
+        exchange
+          .response(resp) // respond to the user  
+          .smartEnd() // end the conversation if appropriate
+          .skipFollowUp();
+      },
+    };
+  }
+}
+
+// export the plugin so it can be used
+module.exports = Performopen2;
